42eb89bd5068483a691a7b303035408a445d0703
Mike014 <mikgrimaldi7@gmail.com>
2026-05-16 18:41:02 +0200

    feat: aggiunto backend Java Spring Boot 3.5.14, entrambi i backend operativi

---
+++ backend-java/src/main/java/com/nexusengine/api/NexusEngineApplication.java
@@ -0,0 +1,13 @@
+package com.nexusengine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusEngineApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/nexusengine/api/NexusEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusengine.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
